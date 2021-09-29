3ce1a22ed87dc9e878ce2cac9308b66d3d1471ec
dikeogwu1m <godsonhm3@gmail.com>
2021-09-29 10:44:06 +0100

    first commit in create account page

---
+++ js/index.js
@@ -0,0 +1,47 @@
+// NAVBAR
+
+// ******** SELECTING DOM ELEMENT ********
+const navToggle = document.querySelector('.nav-toggle')
+const links = document.querySelector('.links')
+const linksWrapper = document.querySelector('.links-wrapper')
+
+// ******** LISTEN FOR EVENT ********
+navToggle.addEventListener('click', () => {
+  // get links height
+  let linksWrapperHeight = linksWrapper.getBoundingClientRect().height
+  let linksHeight = links.getBoundingClientRect().height
+
+  if (linksWrapperHeight == 0) {
+    linksWrapper.style.height = `${linksHeight}px`
+  } else {
+    linksWrapper.style.height = 0
+  }
+})
+
+// FORM
+
+// ******** SELECTING DOM ELEMENT ********
+const form = document.querySelector('.form')
+const alert = document.querySelector('.alert')
+const firstName = document.getElementById('firstName')
+const email = document.getElementById('email')
+const phone = document.getElementById('phone')
+const pass = document.getElementById('pass')
+// ******** LISTEN FOR EVENT ********
+form.addEventListener('submit', (e) => {
+  const emailValue = email.value
+  const firstNameValue = firstName.value
+  const phoneValue = phone.value
+  const passValue = pass.value
+  e.preventDefault()
+  if (!emailValue && !firstNameValue && !phoneValue && !passValue) {
+    showAlert()
+  }
+})
+
+const showAlert = () => {
+  alert.textContent = 'please fill in the forms'
+  setInterval(() => {
+    alert.textContent = ''
+  }, 3000)
+}
